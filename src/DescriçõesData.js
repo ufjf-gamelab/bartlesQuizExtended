0af1fac2bfef5b73f0ded29a12d82dfa24abfa87
João Srbek <joao.srbek@estudante.ufjf.br>
2024-04-17 00:30:13 -0300

    Arruma erros de porteugues nas descrições e arruma o ã de João para Joao

--- src/DescriçõesData.js
+++ src/DescriçõesData.js
@@ -2,74 +2,83 @@ export function getDesc(){
     return {
         Griefer:{
             descrição:`
-                    Jogador de má-fé/Troll, esse tipo de jogador tende a tomar
-                    decisões e fazerem coisas que não fazem muito sentidos para os outros jogadores, até eles
-                    mesmos muitas das vezes não sabem explicar porque eles agem assim ou simplesmente
-                    fingem que aquilo não exige uma explicação, muitos são bem violentos dentro do universo e
-                    tendem a serem “melhores” que os outros além de que suas ações sempre vingam para a
-                    ideia de construir uma má reputação.
-                    `
+                O implícito do Matador é o Jogador de má-fé/Troll, esse tipo de jogador tende a tomar
+                decisões e fazer coisas que não fazem muito sentido para os outros jogadores, até eles
+                mesmos muitas das vezes não sabem explicar porque eles agem assim ou simplesmente
+                fingem que aquilo não exige uma explicação, muitos são bem violentos dentro do universo e
+                tendem a se sentirem “melhores” que os outros, além de que suas ações sempre vingam para a
+                ideia de construir uma má reputação.
+                `
             ,img:'https://cdn-icons-png.flaticon.com/512/556/556209.png'
         },
         Politician:{
             descrição:`
-                esses tipos de jogadores são muito diferentes dos
-                jogadores de má-fé, suas ações são pensadas previamente e também prestam atenção em
-                qual serão as consequências do que eles forem fazer, eles também conseguem explicar
-                oque eles estão fazendo em termos que a comunidade do jogo ira compreender além de
+                O explicito do Matador é o Político, esses tipos de jogadores são muito diferentes dos
+                jogadores de má-fé, suas ações são pensadas previamente e também prestando atenção em
+                quais serão as consequências do que eles forem fazer, eles também conseguem explicar
+                o que eles estão fazendo em termos que a comunidade do jogo irá compreender, além de
                 fazer com que tenham uma boa reputação e na sua ideia mais hostil eles tendem a
-                manipular pessoas para que o ajudem em seus objetivos.
-                    `
+                manipular pessoas para que os ajudem em seus objetivos.
+                `
             ,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiayLOW5E5cRFIVi0kyNY3x2B221haiBJwBMqpiKrptg&s'
         },
         Hacker:{
             descrição:`
-                eles buscam o sentido das coisas serem do jeito que
-                são nos jogos, tendem a intuitivamente entenderem como o mundo do jogo funciona sem
-                necessidade de testar as ideias que eles têm, jogam em busca de fenômenos novos dentro
-                daquele universo`
+                O implícito do Explorador é o Hacker, eles buscam o sentido das coisas 
+                nos jogos com base em outros jogos, tendem a intuitivamente 
+                entenderem como o mundo do jogo funciona sem necessidade de 
+                testar as ideias que eles têm, jogam em busca de fenômenos 
+                novos dentro daquele universo.
+                `
             ,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKDKWu6FgydPP7M_LbWpvnLtR_ckQaLxFyNDtvWooqrw&s'
         },
         
         Scientist:{
             descrição:`
-                eles exploram e experimentam baseados em
-                teorias, testadas previamente por eles mesmos, o Cientista tende a ser metódico na sua
+                O explicito do Explorador é o Cientista, eles exploram e experimentam os jogos, baseados em
+                teorias testadas previamente por eles mesmos, o Cientista tende a ser metódico na sua
                 forma de obter conhecimento dentro do jogo e visa explicar os fenômenos que acontecem
-                no mesmo.`
+                no mesmo.
+                `
             ,img:'https://cdn-icons-png.flaticon.com/512/5010/5010180.png'
         },
         Planner:{
             descrição:`
-                Oportunista, consiste em um jogador que procura
-                várias coisas para fazer em um jogo, mesmo não sabendo oque vão ser essas coisas, eles
-                são poços de ideias e se veem uma oportunidade de fazer algo que os recompensara eles.`
+                O explicito do Conquistador é o Planejador, diferente do Oportunista, o Planejador age
+                pensando no futuro da sua jogatina, suas ações tendem a serem feitas pensando em um
+                resultado tardio e maior do que uma recompensa na hora, eles também tendem a focar em
+                um objetivo específico sem se importar com os desafios ou obstáculos.
+                `
             ,img:'https://cdn4.iconfinder.com/data/icons/business-515/300/103649942Untitled-3-512.png'
         },
         Opportunist:{
             descrição:`
-                planejam como vão fazer para alcançar um objetivo
-                que eles mesmos definiram, eles buscam uma forma de alcançar esse objetivo e planejam
-                como vão fazer isso, eles são metódicos na forma de adquirir conhecimento e buscam
-                entender o jogo de forma mais técnica.`
+                O implícito do Conquistador é o Oportunista, que consiste em um jogador que procura
+                várias coisas para fazer em um jogo, mesmo não sabendo oque vão ser essas coisas, eles
+                são poços de ideias e se veem uma oportunidade de fazer algo que os recompensará eles
+                irão atrás, mas ao mesmo tempo, se há um obstaculo no caminho, eles tendem a desistir
+                do que estão fazendo para fazer algo mais chamativo e agradável.
+                `
             ,img:'https://www.shutterstock.com/image-vector/opportunity-icon-monochrome-simple-business-260nw-2185809673.jpg'
                 
         },
         Network:{
             descrição:`
-                esse tipo de player joga buscando outros
+                O explicito do Socializador é o Networker, esse tipo de player joga buscando outros
                 jogadores específicos para interagir, ele busca saber mais do que todos e saber as
-                informações que eles sabem também, ele busca jogadores considerados por ele uteis o
-                suficiente para poderem ajudar ele em seus objetivos.`
+                informações que eles sabem também, ele busca jogadores considerados por ele úteis o
+                suficiente para poderem ajudar ele em seus objetivos.
+                `
             ,img:'https://w7.pngwing.com/pngs/161/8/png-transparent-computer-icons-asset-management-desktop-business-people-investment-business.png'
         },
         Friend:{
             descrição:`
-                Os amigos são um tipo de jogador que costuma ser
+                O implícito do Socializador é o Amigo, os amigos são um tipo de jogador que costuma ser
                 muito comunicativo com as pessoas e principalmente com quem ele já conhece dentro do
-                jogo, eles entendem como as pessoas pensam e não ligam para suas fraquezas, o objetivo
+                jogo, eles costumam entender como as pessoas pensam e não ligam para suas fraquezas, o objetivo
                 dele no jogo é conhecer mais pessoas para poder se divertir com sua companhia e
-                amizade`
+                amizade.
+                `
             ,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxLCHYpaJJKJn3oU4iGNuYzHCo3oKpnXvvIqWshcxi6Q&s'
         },
     }
